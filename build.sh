23ebca73cf6876e4f2d201790b383659f08e9dd3
Kyle Drake <24930296+icedmoca@users.noreply.github.com>
2022-01-18 06:42:09 -0700

    Updated dl link

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ if [ ! -f "artisan" ]; then
     zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources 2> /dev/null
 
     echo "Downloading the Theme you picked"
-    mkdir -p tempdown && cd tempdown && git clone https://github.com/minenite/paneltheme.git .
+    mkdir -p tempdown && cd tempdown && git clone https://github.com/icedmoca/paneltheme.git .
     cp -r Master/public ..
     cp -r Master/resources ..
 
